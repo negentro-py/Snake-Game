937e68af477662438384e9431ff9f468445e865c
negentro-py <maddiem1402@gmail.com>
2025-03-07 18:32:39 -0500

    bug fix

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
-export default function Navbar() {
+export function Navbar() {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
 
     const handleProfileClick = () => {
         setIsProfileOpen(!isProfileOpen);
         if (!isProfileOpen) {
-          window.open("/profile", "Profile", "width=600,height=400");
+            window.open("/profile", "Profile", "width=600,height=400");
         }
-      };
+    };
     return (
         <div>
             <nav className="flex items center justify-center w-full h-12 bg-[#da935d] px-4">
                 <button className="ml-auto bg-[#9a3bd2] text-white font-semibold rounded px-4 py-1 hover:bg-[#e79995]"
-                onClick={handleProfileClick}>
-          Profile
+                    onClick={handleProfileClick}>
+                    Profile
                 </button>
-            
+
             </nav>
         </div>
     )
