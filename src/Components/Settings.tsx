e0da85d60c483ba7d98328d1fd272c04674a38fc
negentro-py <maddiem1402@gmail.com>
2025-03-11 15:39:58 -0400

    settings.css to components

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -21,7 +21,7 @@ export function Settings() {
     return (
         <section id="settings">
             <button className="settings-container" id="settingsButton" type="button" onClick={openPopup}>
-                <img src="settings.png" alt="settings icon" className="settings-icon"/>
+                <img src="settings.png" alt="settings icon" className="settings-icon" />
             </button>
             <div className={`popup ${isSettingsOpen ? 'open-popup' : ''}`}>
                 <div className="popup-content">
@@ -29,19 +29,19 @@ export function Settings() {
 
                     <div className="popup-content-inner">
                         <div className="music-control">
-                        <h3>Music</h3>
-                        <label className="switch">
-                            <input
-                                type="checkbox"
-                                id="musicSwitch"
-                                checked={isMusicOn}
-                                onChange={toggleMusic}
-                            />
-                            <span className="slider"></span>
-                        </label>
+                            <h3>Music</h3>
+                            <label className="switch">
+                                <input
+                                    type="checkbox"
+                                    id="musicSwitch"
+                                    checked={isMusicOn}
+                                    onChange={toggleMusic}
+                                />
+                                <span className="slider"></span>
+                            </label>
+                        </div>
                     </div>
-                </div>
-                <button id="closeSettings" type="button" onClick={closePopup}>Close</button>
+                    <button id="closeSettings" type="button" onClick={closePopup}>Close</button>
                 </div>
             </div>
         </section>
